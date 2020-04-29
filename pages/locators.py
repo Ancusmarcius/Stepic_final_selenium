276cfc4f37bf987b0abb55e40dd38bcea10fc2fe
Ancusmarcius <alex_aat@mail.ru>
2020-04-29 18:22:17 +0700

    добавили локаторы для формы регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,9 @@ class MainPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_MAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASS = (By.CSS_SELECTOR, "#id_login-password")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+    REG_MAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASS1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_PASS2 = (By.CSS_SELECTOR, "#id_registration-password2")
