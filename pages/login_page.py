3521193257cf2ac826c85ac7c2a851fcc8b03152
Ancusmarcius <alex_aat@mail.ru>
2020-04-29 18:21:55 +0700

    добавили проверку на адресс, на формы

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,7 +12,13 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url, "в адресе отсутствует login"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Форма логина отсутствует"
+
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM) and\
+               self.is_element_present(*LoginPageLocators.LOGIN_MAIL) and\
+               self.is_element_present(*LoginPageLocators.LOGIN_PASS), "проблеммы с формой логина"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Форма Регистрации отсутствует"
+        assert self.is_element_present(*LoginPageLocators.REG_FORM) and\
+               self.is_element_present(*LoginPageLocators.REG_MAIL) and\
+               self.is_element_present(*LoginPageLocators.REG_PASS1) and\
+               self.is_element_present(*LoginPageLocators.REG_PASS2), "проблемы с формой регистрации"
